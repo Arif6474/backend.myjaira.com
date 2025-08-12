ce0108ad755e6ef228a67348b7e9520d14503df1
arif6474 <arifulislam64743@gmail.com>
2025-08-12 20:49:56 +0600

    update customer data

--- routes/consumerAppRoutes/protectedRoutes/protectedRoutes.js
+++ routes/consumerAppRoutes/protectedRoutes/protectedRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { createOrder,  } from '#controllers/order/orderController.js'
 import storeFollowerRoutes from './storeFollowerRoutes.js'
+import { updateProfile } from '#controllers/userControllers/userController.js'
 
 
 const protectedRoutes = Router()
 protectedRoutes.post('/createOrder', createOrder)
+protectedRoutes.patch('/updateProfile', updateProfile)
 protectedRoutes.use('/storeFollower', storeFollowerRoutes)
 
 
